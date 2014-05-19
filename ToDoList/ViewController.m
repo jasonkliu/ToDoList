ec73522a3319e3bb82e31b84108be913749d1e19
Jason Liu <jason@jasonkliu.com>
2014-05-18 17:22:34 -0700

    Add a button and label programatically, change on click

--- ToDoList/ViewController.m
+++ ToDoList/ViewController.m
@@ -16,9 +16,27 @@
 
 - (void)viewDidLoad
 {
+    // Do any additional setup after loading the view, typically from a nib.
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor yellowColor]; // Can also use [UIColor colorWithRed:0.462 green:0.749 blue:0.937 alpha:1.0];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    UIButton *firstButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];  // Create a button
+    firstButton.frame = CGRectMake(100, 100, 100, 44);                          // x, y, width, height
+    [firstButton setTitle:@"Click me!" forState:UIControlStateNormal];          // Normal button view
+    [firstButton setTitle:@"Clicked!" forState:UIControlStateHighlighted];      // Highlighted button view
+    [firstButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside]; // Call buttonPressed
+    [self.view addSubview:firstButton];
+    
+    UILabel *firstLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 30, 200, 44)];
+    firstLabel.text = @"Hello, welcome to my app!";
+    [self.view addSubview:firstLabel];
+}
+
+- (void)buttonPressed:(UIButton *)sender
+{
+    NSLog(@"Button pressed, sender: %@", sender);
+    self.view.alpha = ((double)arc4random() / 0x10000000);      // Randomize the transparency
+    [sender removeFromSuperview];                               // Remove the button
 }
 
 - (void)didReceiveMemoryWarning
